5fb29456fcadf2e7a645eb313d173d26d31c0de4
sadnanrakib <sadnanrakib02@gmail.com>
2022-09-15 11:01:14 +0600

    add

--- script.js
+++ script.js
@@ -110,10 +110,10 @@ const start = () => {
     countdownOverlay.innerHTML =`<h1>${count}</h1>`;
 
     // finished timer
-    if (count < 0) {
+    if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
@@ -135,5 +135,5 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
